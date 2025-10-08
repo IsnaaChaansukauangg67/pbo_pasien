e9e20c4e75747fa13ae9248b5dc254db0e2d5501
IsnaaChaansukauangg67 <isnahamidg35@gmail.com>
2025-10-08 11:37:19 +0700

    Add files via upload

---
+++ Pasien1.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Pertemuan_2.sem3;
+
+/**
+ *
+ * @author isna
+ */
+public class Pasien1 {
+    private final String name;
+    private final int bed;
+    private final String room;
+    private final String condition;
+        
+        
+        
+        
+        //konstruktor
+    public Pasien1(String name,int bed, String room, String condition){
+        this.name = name;
+        this.bed = bed;
+        this.room = room;
+        this.condition = condition;
+    }
+    
+    
+    
+    public void daftarPasien(){
+        System.out.println("NAME        :" + name);
+        System.out.println("NO. BED     :" + bed);
+        System.out.println("LOC.ROOM    :" + room);
+        System.out.println("KONDISI     :" + condition);
+    }
+    }
+            
+
---
+++ daftarPasien1.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Pertemuan_2.sem3;
+
+/**
+ *
+ * @author isna
+ */
+public class daftarPasien1 {
+
+    public static void main(String[] args) {
+        Pasien1 P1 = new Pasien1 ("dwek", 45,"Melati","Kritis");
+        P1.daftarPasien();
+    
+    }
+}
